7790a3659b4e0dae8da30481dc630c620e7dc3c1
Son Sky <125051373+SSkkky@users.noreply.github.com>
2023-12-15 11:02:09 +0900

    fix menu toggle & update observe, aside
    fix css_menu toggle transition
    update css_ menu toggle box-shadow
    update html, js_observe, aside scroll click event

--- js/common.js
+++ js/common.js
@@ -1,17 +1,17 @@
-const burgerBtn = document.querySelector('.burger-button');
-const burgerBtnP = document.querySelectorAll('.burger-button p');
-const burgerMenu = document.querySelector('.burger-menu');
-const burgerMenulist = document.querySelectorAll('.burger-menu .menu');
-const burgerMenuBtn = document.querySelectorAll('.burger-menu .menu img');
-const burgerMenuSub = document.querySelectorAll('.burger-menu .sub');
+const burgerBtn = document.querySelector('.burger-button'),
+burgerBtnP = document.querySelectorAll('.burger-button p'),
+burgerMenu = document.querySelector('.burger-menu'),
+burgerMenulist = document.querySelectorAll('.burger-menu .menu'),
+burgerMenuBtn = document.querySelectorAll('.burger-menu .menu img'),
+burgerMenuSub = document.querySelectorAll('.burger-menu .sub');
 
-const searchBtn = document.querySelector('.search-button');
-const searchBtnImg = document.querySelector('.search-button img');
-const searchMenu = document.querySelector('.search-menu');
+const searchBtn = document.querySelector('.search-button'),
+searchBtnImg = document.querySelector('.search-button img'),
+searchMenu = document.querySelector('.search-menu');
 
-// -------burger menu--------
+// -------burger start--------
 burgerBtn.onclick = function(){
-    burgerMenu.classList.toggle('active');
+    burgerMenu.classList.toggle('toggle');
     this.classList.toggle('active');
     burgerBtnP.forEach(function(el){
         el.classList.toggle('collection');
@@ -27,12 +27,11 @@ burgerMenulist.forEach(function(el, i){
 })
 // -------burger end--------
 
-
-
-// -------search menu--------
+// -------search start--------
 searchBtn.onclick = function(){
     if(searchMenu.classList.contains('active')){
         if(searchMenu.classList.contains('search-2')){
+            // black_background
             searchBtnImg.setAttribute('src','./imgs/ic-search-w.png')
             searchMenu.classList.replace('search-2','search-1');
             searchMenu.classList.remove('active');
@@ -50,3 +49,47 @@ searchBtn.onclick = function(){
     }
 }
 // -------search end--------
+
+// -------aside start-----------
+const elAside = document.querySelector('aside'),
+elSection = document.querySelectorAll('section');
+elArticle = document.querySelectorAll('article');
+
+window.addEventListener('scroll', function(){
+    let scrollTop = window.scrollY
+    // console.log(window.scrollY)
+
+    if(scrollTop >= 700){
+        elAside.classList.add('active');
+    } else {
+        elAside.classList.remove('active');
+    }
+})
+
+elAside.onclick = function (){
+    window.scrollTo({
+        top:0,
+        left:0,
+        behavior:'smooth'
+    }) 
+}
+// -------aside end-----------
+
+//---------observe start----------
+const elObserve = document.querySelectorAll('.observe')
+
+let callback = function(entries, observe){
+    entries.forEach(function(item){
+        if(item.isIntersecting){
+            item.target.classList.add('active');
+            observe.unobserve(item.target)
+        }
+    })
+}
+
+let ob = new IntersectionObserver(callback);
+
+elObserve.forEach(function(item){
+    ob.observe(item);
+})
+//---------observe end----------
